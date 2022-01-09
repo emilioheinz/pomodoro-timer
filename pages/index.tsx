3e308de53998f22a17c7cdceb4164a5fb7530cf3
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-01-09 11:25:53 -0300

    chore: :tada: npx create-next-app and cleanup

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Pomodoro Timer</title>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
