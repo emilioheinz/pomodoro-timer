b331db8c0ac8cf077185984f77839a0a4c636513
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-01-31 20:50:06 -0300

    refactor: :fire: removing intl initialization from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import i18nConfig from '~/config/i18n'
-
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
@@ -9,16 +7,8 @@ import Toast from '~/components/toast'
 import { TimerConfigContextProvider } from '~/contexts/timer-config'
 import { TimerContextProvider } from '~/contexts/timer'
 import { appWithTranslation } from 'next-i18next'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { locale } = useRouter()
-
-  useEffect(() => {
-    i18nConfig.loadGlobalIntlNamespaces(locale as string)
-  }, [locale])
-
   return (
     <ThemeProvider theme={theme}>
       <TimerConfigContextProvider>
